78533801c768352414e143dac2d1453c4d08fee8
AgathaAiko <129515681+AgathaAiko@users.noreply.github.com>
2024-06-12 08:41:34 -0300

    hg

--- main.js
+++ main.js
@@ -1,30 +1,3 @@
 const botoes = document.querySelectorAll(".botao");
 console.log(botoes)
-for(let i=0; i <  botoes.length; i++){
-    console.log(i);
-}
-for(let i=1; i <  botoes.length; i++){
-    console.log(i);
-}
-for(let i=2; i <  botoes.length; i++){
-    console.log(i);
-}
-for(let i=3; i <  botoes.length; i++){
-    console.log(i);
-}
-botoes[i].onclick = function(){
-    
-    botoes[i].classList.add("ativo");
-}
-for(let j=0;j<botoes.length;j++){
-    botoes[j].classList.remove("ativo");
-}
-for(let j=1;j<botoes.length;j++){
-    botoes[j].classList.remove("ativo");
-}
-for(let j=2;j<botoes.length;j++){
-    botoes[j].classList.remove("ativo");
-}
-for(let j=3;j<botoes.length;j++){
-    botoes[j].classList.remove("ativo");
-}
+
