aff4a26756b7ad1eed819378356aae48b884e78e
AgathaAiko <129515681+AgathaAiko@users.noreply.github.com>
2024-06-12 08:36:01 -0300

    dfd

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 const botoes = document.querySelectorAll(".botao");
-console.log(botoes);
+console.log(botoes)
 for(let i=0; i <  botoes.length; i++){
     console.log(i);
+}
+for(let i=1; i <  botoes.length; i++){
+    console.log(i);
+}
+for(let i=2; i <  botoes.length; i++){
+    console.log(i);
+}
+for(let i=3; i <  botoes.length; i++){
+    console.log(i);
+}
+botoes[i].onclick = function(){
+    
+    botoes[i].classList.add("ativo");
 }
